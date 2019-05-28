c73d36c9f343545164f60cc72d4c7d8ece056788
kimura miyuki <hcp.miyuki@gmail.com>
2019-05-28 22:40:32 +0900

    [fix]makeWord2.py

--- makeWord2.py
+++ makeWord2.py
@@ -2,13 +2,13 @@ import itertools
 import pickle
     
 def makeWord2():
-    words = list(map(str, input("please input alphabet>").split()))
+    words = list(input("please input alphabet>"))
     w_count = len(words)
     dic = pickle.load(open("sorted_english_dic.pickle", "rb"))
 
     index = w_count
     result = False
-
+    result_list = []
     for i in range(w_count):
         # 大きい方から組み合わせを作る
         comb_list = list(itertools.combinations(words, index))
@@ -17,17 +17,16 @@ def makeWord2():
             for k, v in dic.items():
                 if k == sorted_comb:
                     result = True
-                    print(v)
+                    result_list.append(v)
                     break
-            
-            if result:
-                break
-        
-        if result:
-            break
         index -= 1
 
     if result == False:
         print("None")
+    else:
+        # 重複している単語を消す
+        unique_result_list = list(dict.fromkeys(result_list))
+        for r in unique_result_list:
+            print(r)
         
 makeWord2()
