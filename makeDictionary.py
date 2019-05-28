7de5a2ce30b9a3a66e631c73a914724967eb09e7
kimura miyuki <hcp.miyuki@gmail.com>
2019-05-28 23:06:40 +0900

    [fix]makeDictionary.py

--- makeDictionary.py
+++ makeDictionary.py
@@ -3,19 +3,26 @@ import requests
 import pickle
 
 ## downloadong dictionary
-url = "https://icanhazwordz.appspot.com/dictionary.words" 
+url = "https://icanhazwordz.appspot.com/dictionary.words"
 response = requests.get(url)
 response.encoding = response.apparent_encoding
 
 english_dic = response.text.split('\n')
+q_english_dic = []
 sorted_english_dic = {}
+
+# quとQuをqに置き換える
+for word in english_dic:
+    q_english_dic.append(word.replace('qu', 'q').replace('Qu', 'q'))
+    
+print(tes_dic)
+
 ## sort
 ### 単語の文字をソート
-for word in  english_dic:
+for word in  q_english_dic:
     sorted_word =  "". join(sorted(word))
     #insert sorted_word to key,original word to value
     sorted_english_dic[sorted_word] = word
 
-    
-print(sorted_english_dic)
+
 pickle.dump(sorted_english_dic, open("sorted_english_dic.pickle", "wb"))
