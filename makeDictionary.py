c363710b3e2e8b3dc5e2a5fbe1ccfa0f2d00daf0
kimura miyuki <hcp.miyuki@gmail.com>
2019-05-28 15:03:03 +0900

    [fix]makeDictionary

--- makeDictionary.py
+++ makeDictionary.py
@@ -8,20 +8,14 @@ response = requests.get(url)
 response.encoding = response.apparent_encoding
 
 english_dic = response.text.split('\n')
-sorted_english = {}
 sorted_english_dic = {}
 ## sort
 ### 単語の文字をソート
 for word in  english_dic:
     sorted_word =  "". join(sorted(word))
     #insert sorted_word to key,original word to value
-    sorted_english[sorted_word] = word
+    sorted_english_dic[sorted_word] = word
 
-### listをkeyでソート
-sorted_english = sorted(sorted_english.items())
-### タプルからdict型に戻す
-for s_word, o_word in sorted_english:
-    sorted_english_dic[s_word] = o_word
     
 print(sorted_english_dic)
 pickle.dump(sorted_english_dic, open("sorted_english_dic.pickle", "wb"))
